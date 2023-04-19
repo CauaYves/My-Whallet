ed5daef84785ee27e93305d6f260b0f10ae7629c
Yves <cauayves5@gmail.com>
2023-04-19 17:06:51 -0300

    route changing on sing-up

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,69 @@
-import { Link } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios"
+import { useState } from "react"
 
 export default function SignUpPage() {
+
+  const navigate = useNavigate()
+
+  //estados
+  const [email, setEmail] = useState("")
+  const [nome, setNome] = useState("")
+  const [senha, setSenha] = useState("")
+  const [confirmSenha, setConfirmSenha] = useState("")
+
+  function register(e) {
+    e.preventDefault()
+
+    axios.post(
+      "https://my-whallet-api.onrender.com/cadastro",
+      {
+        email: email,
+        password: senha
+      }
+    )
+
+    navigate("/")
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={register}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input
+          placeholder="Nome"
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          min={3}
+          autoComplete="new-password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          min={3}
+          autoComplete="new-password"
+          value={confirmSenha}
+          onChange={(e) => setConfirmSenha(e.target.value)}
+        />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
