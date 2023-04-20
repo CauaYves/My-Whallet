3a62ddf08d1273965cdb106d25a34df7f1b2cb82
Yves <cauayves5@gmail.com>
2023-04-19 22:00:52 -0300

    login route request concluded

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,15 +1,44 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { RotateLoader } from "react-spinners"
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate, Link } from "react-router-dom"
 
 export default function SignInPage() {
+
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  async function login(e) {
+    e.preventDefault()
+    setIsLoading(true)
+    await axios.post("https://my-whallet-api.onrender.com/login", {email: email, password: senha})
+    .then((answer) => {
+      console.log(answer)
+      navigate("/home")
+    })
+    .catch((error) => {
+      alert(error.response.statusText)
+    })
+    setIsLoading(false)
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={login}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" autoComplete="new-password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+        <SubmitBtn>
+          {isLoading ?
+            <RotateLoader color="white" />
+            :
+            "entrar"
+          }</SubmitBtn>
       </form>
 
       <Link to="/cadastro">
@@ -25,4 +54,12 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+const SubmitBtn = styled.button`
+  max-height: 48px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
 `
